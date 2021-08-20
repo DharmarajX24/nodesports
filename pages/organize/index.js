dc5780422e8e86a171ad713c39ff4b44ede07f7c
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-20 21:11:12 +0530

    add dynamic route in /organize/tournament/

--- pages/organize/index.js
+++ pages/organize/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import OrganizeComp from "../components/organize/OrganizeComp";
+import OrganizeComp from "../../components/organize/OrganizeComp";
 
 function Organize() {
   return <OrganizeComp />;
