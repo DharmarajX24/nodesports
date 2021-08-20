0ab62a27a625a996b0e355873dedfd960bb0da9a
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-20 12:52:54 +0530

    feat: configure tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import Layout from "../components/common/Layout";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
