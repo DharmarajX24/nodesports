9fc99f6103b395152629522e93d813ff073f740d
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-21 13:32:05 +0530

    feat: add serverside to get user tournament, fix handler to create usertournament

--- pages/organize/index.js
+++ pages/organize/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import OrganizeComp from "../../components/organize/OrganizeComp";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { connectToDatabase } from "../../lib/mongodb";
 
-function Organize() {
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps(context) {
+    const {
+      user: { sub: userId },
+    } = getSession(context.req, context.res);
+    console.log("server side user", userId);
+
+    const { db } = await connectToDatabase();
+    const data = (
+      await db
+        .collection("tournaments")
+        .find({ createdBy: userId }, { projection: { participants: 0 } })
+        .toArray()
+    ).map(({ _id, ...rest }) => ({ id: _id.toString(), ...rest }));
+    // Pass data to the page via props
+    return { props: { data: { ...data } } };
+  },
+});
+
+export default function Organize({ user, data }) {
+  console.log(data);
   return <OrganizeComp />;
 }
-
-export default Organize;
