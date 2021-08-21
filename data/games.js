a9aa642ee3ce7afe90f3b36dafcfa0298dad9d05
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-21 19:58:51 +0530

    feat: add dummy data for game list

---
+++ data/games.js
@@ -0,0 +1,8 @@
+export const games = [
+  { name: "COD", image: "https://ns.dharmaraj.tech/images/call_of_duty.png" },
+  {
+    name: "battlefield",
+    image: "https://ns.dharmaraj.tech/images/battlefield.png",
+  },
+  { name: "fifa", image: "https://ns.dharmaraj.tech/images/fifa.png" },
+];
