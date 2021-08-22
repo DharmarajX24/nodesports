9bc319473b4a7ce2880af20203a97a3a42e843c4
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-22 12:05:34 +0530

    fix: typo in component name

--- components/organize/Manage/ManageTabPanel.js
+++ components/organize/Manage/ManageTabPanel.js
@@ -48,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MangaeTabs({ data }) {
+export default function ManageTabPanel({ data }) {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
