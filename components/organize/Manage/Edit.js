b54e6fc166a68aeb04ae08a51fa5603f0f36f965
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-22 23:21:30 +0530

    feat: add PATCH to update user tournament deatils , tested with 1 field

--- components/organize/Manage/Edit.js
+++ components/organize/Manage/Edit.js
@@ -24,7 +24,12 @@ export default function Edit({ data }) {
     description: "",
     platform: "Xbox",
     region: "ASIA",
-    selectedDate: "2014-08-18T21:11:54",
+    startDate: "2014-08-18T21:11:54",
+    endDate: "2014-08-28T21:11:54",
+    email: "",
+    phone: "",
+    twitter: "",
+    discord: "",
   });
 
   function handleChange(evt) {
@@ -33,37 +38,56 @@ export default function Edit({ data }) {
       ...details,
       [evt.target.name]: value,
     });
-
-    console.log({
-      ...details,
-      [evt.target.name]: value,
-    });
   }
 
+  const updateUserTournament = async () => {
+    const res = await fetch(`/api/tournaments/${data._id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        description: details.description,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const { data: result, error } = await res.json();
+    console.log(result);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateUserTournament();
+  };
   return (
-    <form className={classes.root} noValidate autoComplete="off">
+    <form
+      onSubmit={handleSubmit}
+      className={classes.root}
+      noValidate
+      autoComplete="off"
+    >
       <div className="text-white w-full">
-        <TextField
-          id="standard-multiline-static"
-          label="Description"
-          multiline
-          rows={4}
-          onChange={handleChange}
-          name="description"
-          value={details.description}
-          color="secondary"
-          InputLabelProps={{
-            style: { color: "#EB2B44" },
-          }}
-          InputProps={{
-            style: { color: "#fff" },
-          }}
-          fullWidth
-        />
-        <div className="py-4 mx-2 ">
-          <div className="text-lg">Details</div>
-          <div className="text-xs text-gray-400">Game, platform, region</div>
-          <div className="pb-2">{data.game}</div>
+        <div className="py-4">
+          <div className="text-2xl px-2">About this tournament</div>
+
+          <TextField
+            id="standard-multiline-static"
+            label="Description"
+            multiline
+            rows={4}
+            onChange={handleChange}
+            name="description"
+            value={details.description}
+            color="secondary"
+            InputLabelProps={{
+              style: { color: "#EB2B44" },
+            }}
+            InputProps={{
+              style: { color: "#fff" },
+            }}
+            fullWidth
+          />
+        </div>
+
+        <div className="py-4">
+          <div className="text-2xl px-2">Details</div>
           <div className="flex">
             <TextField
               id="filled-select-currency-native"
@@ -120,23 +144,117 @@ export default function Edit({ data }) {
               ))}
             </TextField>
           </div>
+          <div className="flex">
+            <TextField
+              id="datetime-local"
+              fullWidth
+              name="startDate"
+              label="game date"
+              type="datetime-local"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+                style: { color: "#EB2B44" },
+              }}
+              InputProps={{
+                style: { color: "#fff" },
+              }}
+              color="secondary"
+              value={details.startDate}
+              onChange={handleChange}
+            />
+            <TextField
+              id="datetime-local"
+              name="endDate"
+              label="game date"
+              type="datetime-local"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+                style: { color: "#EB2B44" },
+              }}
+              InputProps={{
+                style: { color: "#fff" },
+              }}
+              fullWidth
+              color="secondary"
+              value={details.endDate}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className="flex py-4 flex-col">
+          <div className="text-2xl px-2">Contact</div>
           <TextField
-            id="datetime-local"
-            name="selectedDate"
-            label="game date"
-            type="datetime-local"
-            className={classes.textField}
+            onChange={handleChange}
+            type="email"
+            name="email"
+            // value={details.email}
+            color="secondary"
             InputLabelProps={{
-              shrink: true,
               style: { color: "#EB2B44" },
             }}
             InputProps={{
               style: { color: "#fff" },
             }}
-            color="secondary"
-            value={details.selectedDate}
-            onChange={handleChange}
+            id="standard-basic"
+            label="email"
           />
+          <TextField
+            onChange={handleChange}
+            name="phone"
+            type="tel"
+            // value={details.phone}
+            color="secondary"
+            InputLabelProps={{
+              style: { color: "#EB2B44" },
+            }}
+            InputProps={{
+              style: { color: "#fff" },
+            }}
+            id="standard-basic"
+            label="phone"
+          />
+          <TextField
+            onChange={handleChange}
+            name="twitter"
+            type="url"
+            // value={details.twitter}
+            color="secondary"
+            InputLabelProps={{
+              style: { color: "#EB2B44" },
+            }}
+            InputProps={{
+              style: { color: "#fff" },
+            }}
+            id="standard-basic"
+            label="twitter"
+          />
+          <TextField
+            onChange={handleChange}
+            name="discord"
+            type="url"
+            // value={details.discord}
+            color="secondary"
+            InputLabelProps={{
+              style: { color: "#EB2B44" },
+            }}
+            InputProps={{
+              style: { color: "#fff" },
+            }}
+            id="standard-basic"
+            label="discord"
+          />
+        </div>
+
+        <div className="p-2 flex justify-end">
+          <button
+            className="p-2 rounded bg-secondarybranding hover:text-secondarybranding hover:bg-white"
+            type="submit"
+          >
+            Update
+          </button>
         </div>
       </div>
     </form>
