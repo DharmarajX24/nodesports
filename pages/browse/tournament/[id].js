d2249ffb8d568ab1fcf9a3a3a6aa07a6f4a05154
Dharmaraj <63334359+DharmarajX24@users.noreply.github.com>
2021-08-24 23:02:23 +0530

    feat: db query on view tournament page

--- pages/browse/tournament/[id].js
+++ pages/browse/tournament/[id].js
@@ -1,17 +1,28 @@
-// import { useRouter } from "next/router";
-// import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
-// import { connectToDatabase } from "../../../lib/mongodb";
-// import { ObjectId } from "mongodb";
+import { useRouter } from "next/router";
+import { connectToDatabase } from "../../../lib/mongodb";
+import { ObjectId } from "mongodb";
 
+export const getServerSideProps = async (context) => {
+  const { tournamentId } = context.query;
+
+  const { db } = await connectToDatabase();
+
+  const data = await db
+    .collection("tournaments")
+    .findOne(
+      { _id: new ObjectId(tournamentId) },
+      { projection: { createdBy: 0 } }
+    );
+
+  if (!data) return { notFound: true };
+  // Pass data to the page via props
+  return { props: { data: JSON.parse(JSON.stringify(data)) } };
+};
 
 export default function BrowseTournamentPage({ data }) {
   const router = useRouter();
   console.log(router.query);
   const { id } = router.query;
-  console.log({ data });
-  return (
-    <div className="py-10">
-        {}
-    </div>
-  );
+  console.log(data);
+  return <div className="py-10">{}</div>;
 }
