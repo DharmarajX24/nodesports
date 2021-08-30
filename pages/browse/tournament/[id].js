e7aaefba8b22e9096212b9c3890e1fcbbd4ecba1
DharmarajX24 <63334359+DharmarajX24@users.noreply.github.com>
2021-08-30 19:47:11 +0530

    re-add participants array

--- pages/browse/tournament/[id].js
+++ pages/browse/tournament/[id].js
@@ -26,7 +26,7 @@ export const getServerSideProps = async (context) => {
         JSON.stringify({
           ...rest,
           isParticipant: user && participants.includes(user.sub.split("|")[1]),
-          totalParticipants: participants.length,
+          participants,
         })
       ),
     },
