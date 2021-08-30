dead978e5896017ab27946769a3d843f6008fa1a
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-30 19:58:30 +0530

    feat: replace dummy data of date and location in tournament card with actual data

--- components/common/TournamentCards.js
+++ components/common/TournamentCards.js
@@ -4,12 +4,15 @@ import clashRoyale from "../../public/cr.jpg";
 import Link from "next/link";
 import Calendar from "../icons/Calendar";
 import Location from "../icons/Location";
-
+import { unixToMaterialUi } from "../../handlers/date-helper"
+import { isObjectEmpty } from '../../handlers/utility'
+ 
 function TournamentCards({ data, baseRoute }) {
+  console.log({data})
   return (
     <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 pt-6 px-4">
-      {data.map((item) => (
-        <Link href={`/${baseRoute}/tournament/${item._id}`} key={item._id}>
+      {data.map((card) => (
+        <Link href={`/${baseRoute}/tournament/${card._id}`} key={card._id}>
           <a className="bg-secondary p-4 hover:shadow-game">
             <div className="flex items-center pb-4">
               <Image
@@ -20,19 +23,19 @@ function TournamentCards({ data, baseRoute }) {
                 src={clashRoyale}
               />
               <div className="px-2">
-                <div className=" font-bold">{item.name}</div>
-                <div className="font-light">{item.game}</div>
+                <div className=" font-bold">{card.name}</div>
+                <div className="font-light">{card.game}</div>
               </div>
             </div>
             <div className="px-2">
               <div className="flex pb-4">
                 <Calendar />
-                <div className="pl-10 text-lg font-medium">28 AUG 2021</div>
+                <div className="pl-10 text-lg font-medium">{card.time.start === 0  || isObjectEmpty(card.time) ? "TBD" : unixToMaterialUi(card.time.start)}</div>
               </div>
               <div className="flex">
                 <Location />
-                <div className="pl-10 text-lg font-medium">AMERICA</div>
-              </div>
+                <div className="pl-10 text-lg font-medium">{data.region || 'TBD'}</div>
+              </div>  
             </div>
           </a>
         </Link>
--- components/organize/Manage/Overview.js
+++ components/organize/Manage/Overview.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import Hero from "../../../public/cod.png";
+import { unixToMaterialUi } from "../../../handlers/date-helper";
+import { isObjectEmpty } from "../../../handlers/utility"
 
 function Overview({ data }) {
   return (
     <div>
-      <Image src={Hero} height="400" />
+      <Image src={Hero} alt='' height="400" />
 
       <div className="px-2 py-4">
         <div className="py-2 font-semibold text-2xl">About this tournament</div>
@@ -23,22 +25,44 @@ function Overview({ data }) {
         <div className="pt-6">
           <div className="text-xs text-gray-400">Date and time</div>
           <div className="">
-            {data.time.start !== 0 ? data.time.start : "Schedule unavailable"}
+            {data.time.start !== 0
+              ? unixToMaterialUi(data.time.start)
+              : "Schedule unavailable"}
+            <br />
+            {data.time.start !== 0 && unixToMaterialUi(data.time.end)}
           </div>
         </div>
       </div>
-
-      <div className="py-4 px-2">
+      {isObjectEmpty(data.contact) ? null :  <div className="py-4 px-2">
         <div className="py-2 font-semibold text-2xl">Contact</div>
-        <div className="text-xs text-gray-400">
-          email, phone, twitter, discord
-        </div>
+        {data.contact.email ? (
+          <>
+            <div className="text-xs text-gray-400">email</div>
+            <div>{data.contact.email}</div>
+          </>
+        ) : null}
+        {data.contact.phone ? (
+          <>
+            <div className="text-xs text-gray-400">phone</div>
+            <div>{data.contact.phone}</div>
+          </>
+        ) : null}
 
-        <div className="">
-          {data.contact.email} | {data.contact.phone} | {data.contact.twitter} |{" "}
-          {data.contact.discord}{" "}
-        </div>
-      </div>
+        {data.contact.twitter ? (
+          <>
+            <div className="text-xs text-gray-400">twitter</div>
+            <div>{data.contact.twitter}</div>
+          </>
+        ) : null}
+
+        {data.contact.discord ? (
+          <>
+            <div className="text-xs text-gray-400">discord</div>
+            <div>{data.contact.discord}</div>
+          </>
+        ) : null}
+      </div>}        
+     
     </div>
   );
 }
