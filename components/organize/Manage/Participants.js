c9ffd8f95ce3ba1a2d84425e33640e429877054b
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-31 22:20:19 +0530

    faet: add leaderboard tab in manage torunamennt

--- components/organize/Manage/Participants.js
+++ components/organize/Manage/Participants.js
@@ -1,35 +1,34 @@
-  import Button from "@material-ui/core/Button";
+import Button from "@material-ui/core/Button";
 import { useState } from "react";
 
-function Participants({ data,id }) {
-  const [participants, setParticipants] = useState(data.participants)
+function Participants({ data, id }) {
+  const [participants, setParticipants] = useState(data.participants);
   const isParticipants = participants.length;
   if (!isParticipants) return <div>No one has joined your tournament yet.</div>;
 
-  const removeUser = async (person,index) => {
+  const removeUser = async (person, index) => {
     const res = await fetch(`/api/tournaments/${id}/participants`, {
-      body: JSON.stringify({userId: person}),
+      body: JSON.stringify({ userId: person }),
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
     });
     const { data: result, error } = await res.json();
     console.log(result);
-    if(!error)
-    setParticipants(participants.splice(index, 1))
+    if (!error) setParticipants(participants.splice(index, 1));
   };
 
   return (
     <div className="">
       <div className="p-2">Participants tab</div>
       <div>
-        {participants.map((person,i) => (
+        {participants.map((person, i) => (
           <div
             className="bg-secondary p-2 my-2 grid grid-cols-searchBarAndBtnColums"
             key={person}
           >
             <div>{person}</div>
             <Button
-              onClick={() => removeUser(person,i)}
+              onClick={() => removeUser(person, i)}
               variant="contained"
               color="primary"
             >
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import Layout from "../components/common/Layout";
@@ -9,35 +9,27 @@ import theme from "../styles/material.ui.theme.provider";
 export default function App({ Component, pageProps }) {
   return (
     <>
-     <Head>
+      <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <title>Nodesports</title>
 
         <meta property="og:image" content={"/static/logo.png"} key="ogimage" />
-        <meta
-          property="og:site_name"
-          content="Nodesports"
-          key="ogsitename"
-        />
-        <meta
-          property="og:title"
-          content="Nodesports"
-          key="ogtitle"
-        />
+        <meta property="og:site_name" content="Nodesports" key="ogsitename" />
+        <meta property="og:title" content="Nodesports" key="ogtitle" />
         <meta
           property="og:description"
           content="Esports platform to host and play tournaments"
           key="ogdesc"
         />
       </Head>
-    <ThemeProvider theme={theme}>
-      <UserProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </UserProvider>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <UserProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </UserProvider>
+      </ThemeProvider>
     </>
   );
 }
