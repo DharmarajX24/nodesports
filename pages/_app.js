bed2ea0e8cab192d58404850530a5da18d130014
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-31 21:58:06 +0530

    feat: add og tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-// pages/_app.js
+import Head from 'next/head'
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0";
 import Layout from "../components/common/Layout";
@@ -8,6 +8,29 @@ import theme from "../styles/material.ui.theme.provider";
 
 export default function App({ Component, pageProps }) {
   return (
+    <>
+     <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>Nodesports</title>
+
+        <meta property="og:image" content={"/static/logo.png"} key="ogimage" />
+        <meta
+          property="og:site_name"
+          content="Nodesports"
+          key="ogsitename"
+        />
+        <meta
+          property="og:title"
+          content="Nodesports"
+          key="ogtitle"
+        />
+        <meta
+          property="og:description"
+          content="Esports platform to host and play tournaments"
+          key="ogdesc"
+        />
+      </Head>
     <ThemeProvider theme={theme}>
       <UserProvider>
         <Layout>
@@ -15,5 +38,6 @@ export default function App({ Component, pageProps }) {
         </Layout>
       </UserProvider>
     </ThemeProvider>
+    </>
   );
 }
