53458d87658ec2b5e7bf6fdb8dd82b7858d4a1b9
Dharmaraj <63334359+DharmarajX24@users.noreply.github.com>
2022-08-19 22:15:42 +0530

    installed Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
-import { defineNuxtConfig } from 'nuxt'
+import {defineNuxtConfig} from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          autoprefixer: {},
+          tailwindcss: {}
+        }
+      }
+    },
+  },
+  css: [
+    '@/assets/css/main.css',
+  ],
   modules: [
     '@pinia/nuxt'
   ]
---
+++ pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Login page</h1>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
